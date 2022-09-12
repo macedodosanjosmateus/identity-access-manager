7857e05e88c5a849697cd248da78d5f4f74239eb
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-09-12 07:39:27 -0300

    refactor: predefined variables

--- src/user/service/user.service.spec.ts
+++ src/user/service/user.service.spec.ts
@@ -1,5 +1,6 @@
 import { UserMock } from '@/test/mock/user/user.mock'
 import { CreateUserCommand } from '@/user/command/create-user/create-user.command'
+import { TUserData } from '@/user/contract/user.type'
 import { UserService } from '@/user/service/user.service'
 import { CommandBus, QueryBus } from '@nestjs/cqrs'
 import { Test, TestingModule } from '@nestjs/testing'
@@ -8,6 +9,7 @@ describe('UserService', () => {
   let service: UserService
   let commandBus: CommandBus
   let queryBus: QueryBus
+  let userData: TUserData
   let mocker: UserMock
   beforeEach(async () => {
     jest.clearAllMocks()
@@ -43,15 +45,15 @@ describe('UserService', () => {
 
   describe('create', () => {
     it('should create and execute a create user command', async () => {
-      const data = mocker.createUserData()
-      await service.createUser(data)
-      expect(commandBus.execute).toBeCalledWith(new CreateUserCommand(data))
+      userData = mocker.createUserData()
+      await service.createUser(userData)
+      expect(commandBus.execute).toBeCalledWith(new CreateUserCommand(userData))
       expect(commandBus.execute).toBeCalledTimes(1)
     })
     it('should throw a error if command bus throws', async () => {
-      const data = mocker.createUserData()
+      userData = mocker.createUserData()
       commandBus.execute = jest.fn().mockRejectedValue(new Error('error'))
-      await expect(service.createUser(data)).rejects.toThrowError('error')
+      await expect(service.createUser(userData)).rejects.toThrowError('error')
     })
   })
 })
