496601deaf0ce01e41cf52d953a61d66b9580255
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-09-12 03:14:13 -0300

    refactor: load config inside module

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,8 +1,7 @@
+import { CommonModule } from '@/common/common.module'
+import { DatabaseModule } from '@/database/database.module'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { CommonModule } from '@/common/common.module'
-import { appConfig } from '@/config/app.config'
-import { DatabaseModule } from '@/database/database.module'
 
 @Module({
   imports: [
@@ -10,9 +9,9 @@ import { DatabaseModule } from '@/database/database.module'
       ignoreEnvFile: process.env.NODE_ENV === 'production',
       envFilePath: '.env',
       expandVariables: process.env.NODE_ENV !== 'production',
-      cache: true
+      cache: true,
+      isGlobal: true
     }),
-    ConfigModule.forFeature(appConfig()),
     CommonModule,
     DatabaseModule
   ]
