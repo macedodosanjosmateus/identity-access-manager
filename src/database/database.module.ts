f2059f068b5ebf460ecbea6fcbabf36f63935ba7
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-09-12 03:14:47 -0300

    chore: orm database settings module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,26 @@
-import { Module } from '@nestjs/common';
+import { sqlConfig } from '@/config/sql.config'
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forFeature(sqlConfig()),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get<string>('sql.host'),
+          port: configService.get<number>('sql.port'),
+          username: configService.get<string>('sql.username'),
+          password: configService.get<string>('sql.password'),
+          database: configService.get<string>('sql.database'),
+          entities: [],
+          synchronize: configService.get<boolean>('sql.synchronize')
+        }
+      }
+    })
+  ]
+})
 export class DatabaseModule {}
