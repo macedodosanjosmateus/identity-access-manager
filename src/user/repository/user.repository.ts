9efd18b92636f0416d644f085d2c36676b0d896a
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-09-12 08:18:53 -0300

    feat: ensure user repository create a user entity and wrapper on user aggregator

---
+++ src/user/repository/user.repository.ts
@@ -0,0 +1,17 @@
+import { UserEntity } from '@/database/models/user.entity'
+import { UserAggregate } from '@/user/aggregate/user.aggregate'
+import { TUserData } from '@/user/contract/user.type'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+
+export class UserRepository {
+  constructor(@InjectRepository(UserEntity) private readonly repository: Repository<UserEntity>) {}
+
+  async createUser(data: TUserData): Promise<UserAggregate> {
+    const userEntity = new UserEntity()
+    userEntity.email = data.email
+    userEntity.mobile = data.mobile
+    await this.repository.save(userEntity)
+    return new UserAggregate(userEntity)
+  }
+}
