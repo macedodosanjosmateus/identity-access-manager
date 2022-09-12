1106fc5886c05d8a6ce1f0f24893667de04042ff
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-09-12 09:15:40 -0300

    feat: ensure user service create and execute a find users query

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,5 +1,6 @@
 import { CreateUserCommand } from '@/user/command/create-user/create-user.command'
-import { TUserData } from '@/user/contract/user.type'
+import { TUser, TUserData } from '@/user/contract/user.type'
+import { FindUsersQuery } from '@/user/query/find-users/find-users.query'
 import { Injectable } from '@nestjs/common'
 import { CommandBus, QueryBus } from '@nestjs/cqrs'
 
@@ -9,5 +10,9 @@ export class UserService {
 
   async createUser(data: TUserData): Promise<void> {
     await this.commandBus.execute(new CreateUserCommand(data))
+  }
+
+  async findAllUsers(): Promise<TUser[]> {
+    return this.queryBus.execute(new FindUsersQuery())
   }
 }
