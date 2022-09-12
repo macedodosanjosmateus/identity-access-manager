b5bb6bfaf8d19c30237da904c5615e6e9a8d0ec5
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-09-12 09:30:23 -0300

    test: ensure user aggregate get formatted data from with corrected values

--- src/user/aggregate/user.aggregate.spec.ts
+++ src/user/aggregate/user.aggregate.spec.ts
@@ -1,3 +1,4 @@
+import { UserEntity } from '@/database/models/user.entity'
 import { UserMock } from '@/test/mock/user/user.mock'
 import { UserAggregate } from '@/user/aggregate/user.aggregate'
 import { TUserData } from '@/user/contract/user.type'
@@ -5,6 +6,7 @@ import { UserCreatedEvent } from '@/user/event/user-created/user-created.event'
 
 describe('UserAggregate', () => {
   let userAggregate: UserAggregate
+  let userEntity: UserEntity
   let userData: TUserData
   let mocker: UserMock
   describe('create', () => {
@@ -16,6 +18,22 @@ describe('UserAggregate', () => {
       userAggregate.create(userData)
       expect(spy).toBeCalledWith(new UserCreatedEvent(userData))
       expect(spy).toBeCalledTimes(1)
+    })
+  })
+
+  describe('getFormattedData', () => {
+    it('should get formatted data from with corrected values', () => {
+      mocker = new UserMock()
+      userData = mocker.createUserData()
+      userEntity = mocker.createUserEntity(userData)
+      userAggregate = new UserAggregate(userEntity)
+      expect(userAggregate.getFormattedData()).toEqual({
+        id: userEntity.id,
+        email: userEntity.email,
+        mobile: userEntity.mobile,
+        createdAt: userEntity.createdAt,
+        updatedAt: userEntity.updatedAt
+      })
     })
   })
 })
