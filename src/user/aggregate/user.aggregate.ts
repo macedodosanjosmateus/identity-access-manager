9749feed987424f3fe76cc2f6a13d7f24dbea63a
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-09-12 09:29:52 -0300

    feat: ensure user aggregate get formatted data from with corrected values

--- src/user/aggregate/user.aggregate.ts
+++ src/user/aggregate/user.aggregate.ts
@@ -1,5 +1,5 @@
 import { UserEntity } from '@/database/models/user.entity'
-import { TUserData } from '@/user/contract/user.type'
+import { TUser, TUserData } from '@/user/contract/user.type'
 import { UserCreatedEvent } from '@/user/event/user-created/user-created.event'
 import { AggregateRoot } from '@nestjs/cqrs'
 
@@ -8,7 +8,20 @@ export class UserAggregate extends AggregateRoot {
     super()
   }
 
-  create(data: TUserData) {
+  getFormattedData(): TUser {
+    if (!this.user) {
+      throw new Error('User is not defined')
+    }
+    return {
+      id: this.user.id,
+      email: this.user.email,
+      mobile: this.user.mobile,
+      createdAt: this.user.createdAt,
+      updatedAt: this.user.updatedAt
+    }
+  }
+
+  async create(data: TUserData): Promise<void> {
     this.apply(new UserCreatedEvent(data))
   }
 }
